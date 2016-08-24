bd416ccf1ac0da75b343149a9c4252a774a0c245
Dmitriy Arkhipov <dmitriy.arkhipov.biz@mail.ru>
2016-08-24 16:42:14 +0800

    Initial Commit

---
+++ pool_object_pattern/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pool_object_pattern
+//
+//  Created by Dmitriy Arkhipov on 24.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ pool_object_pattern/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pool_object_pattern
+//
+//  Created by Dmitriy Arkhipov on 24.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
